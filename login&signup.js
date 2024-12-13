1ed1e8e4317b76a83beca9f6ed5bf700dcd160d7
Kishore <leninkishore25@gmail.com>
2024-12-13 22:19:53 +0530

    changes

--- login&signup.js
+++ login&signup.js
@@ -9,10 +9,8 @@ var s = document.getElementById("b2");
 
 r.onclick = function ()
 {
-    d.classList.add("mover");
     a.classList.add("hide");
     a.classList.remove("show");
-    c.classList.add("movel2");
     b.classList.add("hide");
     b.classList.remove("show");
     c.classList.remove("hide");
@@ -22,10 +20,8 @@ r.onclick = function ()
 }
 s.onclick = function ()
 {
-    b.classList.add("mover2");
     c.classList.add("hide");
     c.classList.remove("show");
-    a.classList.add("movel");
     d.classList.add("hide");
     d.classList.remove("show");
     b.classList.remove("hide");
